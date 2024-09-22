0417cb4e6e8c04512195afba94aae8443c1892e4
boru-to <borutovortex49@gmail.com>
2024-09-22 18:14:47 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -11,18 +11,18 @@ smd({
   filename: __filename
 }, async (message) => {
   try {
-    const response = await axios.get("https://api.github.com/repos/hakisolos/Queen_Nikka");
+    const response = await axios.get("https://api.github.com/repos/boru-to/KATAKURI_MD");
     const repoData = response.data;
 
-    let replyMessage = `**🍀WhatsApp Bot Repository Info**\n\n`;
-    replyMessage += `**🍀Repository Name:** ${repoData.name}\n`;
-    replyMessage += `**🍀Description:** ${repoData.description || 'No description available'}\n`;
-    replyMessage += `**🍀Stars:** ${repoData.stargazers_count}\n`;
-    replyMessage += `**🍀Forks:** ${repoData.forks_count}\n`;
-    replyMessage += `**🍀Watchers:** ${repoData.watchers_count}\n`;
-    replyMessage += `**🍀Open Issues:** ${repoData.open_issues_count}\n`;
-    replyMessage += `**🍀License:** ${repoData.license ? repoData.license.name : 'No License'}\n`;
-    replyMessage += `**🍀Repository URL:** [Click here]👉(https://github.com/hakisolos/Queen_Nikka)\n`;
+    let replyMessage = `**✨WhatsApp Bot Repository Info**\n\n`;
+    replyMessage += `**✨Repository Name:** ${repoData.KATAKURI_MD\n`;
+    replyMessage += `**✨Description:** ${repoData.I AM KATAKURI_MD A WHATSAPP BOT ARISED BY KATAKURI|| 'No description available'}\n`;
+    replyMessage += `**✨Stars:** ${repoData.stargazers_count}\n`;
+    replyMessage += `**✨Forks:** ${repoData.forks_count}\n`;
+    replyMessage += `**✨Watchers:** ${repoData.watchers_count}\n`;
+    replyMessage += `**✨Open Issues:** ${repoData.open_issues_count}\n`;
+    replyMessage += `**✨License:** ${repoData.license ? repoData.license.name : 'No License'}\n`;
+    replyMessage += `**✨Repository URL:** [Click here]👉(https://github.com/boru-to/KATAKURI_MD)\n`;
 
     await message.send(replyMessage);
   } catch (error) {
