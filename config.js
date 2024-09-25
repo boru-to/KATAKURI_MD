0eb099e5e7f451e13e04b8abe31a12043f2ff5f7
boru-to <borutovortex49@gmail.com>
2024-09-25 19:58:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Pretoria";
 global.github = process.env.GITHUB || "https://github.com/boru-to/KATAKURI_MD";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vaf5x0eHFxP7JvSRRn1g";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vaf5x0eHFxP7JvSRRn1g";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://files.catbox.moe/23fcdx.jpg";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://files.catbox.moe/hm4h9v.jpg";
 global.devs = "27747815326";
 global.sudo = process.env.SUDO || "27747815326";
 global.owner = process.env.OWNER_NUMBER || "27747815326";
@@ -27,7 +27,7 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
-global.userImages = process.env.USER_IMAGES || "https://files.catbox.moe/23fcdx.jpg";
+global.userImages = process.env.USER_IMAGES || "https://files.catbox.moe/hm4h9v.jpg";
 global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
@@ -45,7 +45,7 @@ global.SESSION_ID =
   "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUhGNG50S0w4UFNXUWtMMUV4azl5akltUTVQRThLblBnSWY2M0orRk1Fbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTEhuM0ZHQXZjeXY5RUNJdEhXRkJNTnJOUXlFbHJ1NFhRYnQ1Zms1Z25rMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJPQmpOckwxKzN0WnRIbjE1N091N0huK2tmTnZlUy9RQysrZ3F3VjRRWFdrPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzMlZUYW9XL3dBSUlDNWNsOE1QajM0TllSWTJtZm5LdU90blVuMGlxbVI0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImlJWUl4NXJML2dPSXRLY2l6K1RnZ2xJSHZYR1prVDBtNDFpbGVYdlF5RjA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdFb0ZNcTNTRzV1V0JGNWNGVkhkUHdHSlo0MGowdTRsNnFpMzQ2QmRTSHc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNlBtK1JBRks4RDBaaE5YalhkQW1JODloL3VYYXlVcEo0QkRqNWtVSTFHYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOEpnN3c1WWM2SnpYL3lna3lJeWhkOUJFRHVkZEMrak5JVGpGTlIvMW53dz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkRmUnZLTFpFcnZUTDdNU0Y2b2wxUU5kTGJUZjBVeXhHRlRyNGRPYXZnMWVzZTZkb09MdnEvWTZPTm1yT2dNdzVYMUdMOUd3WTEreU9zRHpSb0VaZ0FBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTYsImFkdlNlY3JldEtleSI6IklxZXlXTzlxZkkvbUc5WlpXTng3Y0FFcUhScVZVODVhL0U3eEhRMXh1Z2M9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IlJMcFZtTVFRVF9lNjlzRlNXODlPRnciLCJwaG9uZUlkIjoiZDhjMzk4YTAtZjIyMS00N2JiLTgyNTYtYmRkZmY1ZGZlMTVjIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InJxWWp2bXdhL3FrNi9TR1dObzY4N0k0dUFCOD0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsZ05ZaUNKUFZwZ3FjQy9kbUdOb2M4OFJ1UjQ9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiRDc5VDFFV1IiLCJtZSI6eyJpZCI6IjI3NzQ4MjU1ODQ4OjJAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0ptYWhjVUhFSnVrdmJjR0dBSWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IjREbFg0RllYWTNyNVlvaG5UbUpzc0ZpMkNqZUpLc25NbnJFTkN6N0w4aTQ9IiwiYWNjb3VudFNpZ25hdHVyZSI6Im8rVHNGL2ZsT0ZiMENXa2Z2MFJDUjhMMXZXUGdEVDZzNlRaV3A5QTZJaElRcjYvMUZRYWkwdHBtM3V2MW15Mi84RUk4MFk5VittTTFnWUtqNWRhTkF3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJNU2hlcnNDOWpsM3VCbWREN0hmWDRJaEtOK1pOekh1a0htUHFTY1Jza292SEJnbFRGcy9MMnh3dWRWQmNEVHJKOUl6WkZHK0g1dVlmaDhMOXBjRnhCZz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI3NzQ4MjU1ODQ4OjJAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZUE1VitCV0YyTjYrV0tJWjA1aWJMQll0Z28zaVNySnpKNnhEUXMreS9JdSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyNjk2MDE2OSwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFCdEIifQ=="
 module.exports = {
   menu: process.env.MENU || "2",
-  HANDLERS: process.env.PREFIX || "+./!,# ",
+  HANDLERS: process.env.PREFIX || "+",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`KATAKURI_MD™`",
